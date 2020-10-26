ab2509b4c73649a102827339b1a834a46d21db8d
Afshin Rahmati <afshinrahmati93@gmail.com>
2020-10-26 15:50:21 -0400

    collps

--- public/src/js/admin.js
+++ public/src/js/admin.js
@@ -2,27 +2,36 @@ $(".box-open").hide();
 $(".zir-majmo").hide();
 $(".fa-angle-down").hide();
 $(".menu-show").on("click", function ali() {
-    $(".a-men").hide(); 
-    $(".menu-show").hide(); 
+    $(".a-men").hide();
+    $(".menu-show").hide();
+    $("#collapseOne").hide();
+    $(".x1111").hide();
     $(".box-open").show();
-    $(".menu").css("width", "5%");  
+    $(".menu").css("width", "5%");
 });
 $(".box-open").on("click", function ali() {
-    $(".a-men").show(); 
-    $(".menu-show").show(); 
+    $(".a-men").show();
+    $("#collapseOne").show();
+
+
+    $(".cople_slide").show();
+    $(".menu-show").show();
     $(".box-open").hide();
-    $(".menu").css("width", "18%");  
+    $(".menu").css("width", "18%");
 });
 $(".cople").on("click", function ali() {
     $(".zir-majmo").show();
+
     $(".fa-angle-down").show();
     $(".fa-angle-left").hide();
-    
+
+
+
 });
 $(".fa-angle-down").on("click", function ali() {
-   
+    $("#collapseOne").show();
     $(".zir-majmo").hide();
     $(".fa-angle-down").hide();
     $(".fa-angle-left").show();
-    
+
 });
